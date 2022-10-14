eb8a7bc16467833842e002be7f86ae5c13a07386
Yingjie Jia <julieyingjie@gmail.com>
2022-10-14 01:34:37 -0400

    modified:   courseReport.php 	modified:   indexAdmin.php 	modified:   studentReport.php

--- courseReport.php
+++ courseReport.php
@@ -1,1 +1,77 @@
-need the enroll table 
+<!DOCTYPE html>
+<html>
+    <title>course report Page6</title>
+    <body>
+   
+
+    <h1> The Following Student are on this courses !</h1>  
+ 
+<?php
+
+    function display_registeredStudent(){
+    include_once 'connection.php';
+    extract( $_POST);
+    // session_start();
+    // $sID = $_SESSION['sID'];
+    // extract($_POST);
+
+    $query = "SELECT * 
+              FROM enrolled, student 
+              WHERE enrolled.cID = $crID AND enrolled.sID = student.sID ";
+    
+    if($result = $conn->query($query)){
+        while($row = $result->fetch_assoc()) {
+            $sID = $row["sID"];
+            $fName = $row["fName"];
+            $lName = $row["lName"];
+            $address = $row["address"];
+            $email = $row["email"];
+            $phone = $row["phone"];
+            $dOB = $row["dOB"];
+           
+
+            $studentList = <<<DELIMETER
+            <tr role="row">
+            <td row="cell">{$sID}</td>
+            <td row="cell">{$fName}</td>
+            <td row="cell">{$lName}</td>
+            <td row="cell">{$address}</td>
+            <td row="cell">{$email}</td>
+            <td row="cell">{$phone}</td>
+            <td row="cell">{$dOB}</td>
+            </tr> 
+            DELIMETER;
+
+        echo $studentList;
+        //echo "$cID";
+        //echo "$sID";
+        // echo "$row[cID]";
+            
+        }
+
+        $result->free();
+    } 
+
+}
+
+?>
+
+<table class = "courseTable">
+        <tr>
+            <th>sID</th>
+            <th>fName</th>
+            <th>lName</th>
+            <th>address</th>
+            <th>email</th>
+            <th>phone</th>
+            <th>dOB</th>
+        </tr>
+        <?php
+        display_registeredStudent();
+        ?>
+    </table>
+
+
+</body>
+</html>
+
--- indexAdmin.php
+++ indexAdmin.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <title>Administrator Management Page Page6</title>
+    <title>admin page Page6</title>
     <body>
 
-        <h1>Welcome Administrator!</h1>
-        <?php 
+        <h1> Welcome </h1>
+          <?php 
             include_once 'connection.php';
             extract( $_POST);
 
-            $sql = "select fName, lName FROM Administrator WHERE eID = ". $eID;
+            $sql = "select fName, lName FROM administrator WHERE eID = ". $eID;
             $result = $conn->query($sql);
             //connect to MySQL
-
             
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
-                  echo "id: " . $eID . "  Name: " . $row["fName"]. " " . $row["lName"]. "<br>";
+                  echo  $row["fName"]. " " . $row["lName"]. "<br>";
                 }
               } else {
                 echo "0 results";
@@ -25,7 +24,27 @@
         ?>
 
 
+        <form method post = "post" action = "courseCreation.php">
+              <div>
+                <h2>Click here to create a new course</h2>
+                <input type = "submit" value = "create">
+              </div>
+        </form>
+
         
+        <!--a list of course taken by a certain student -->
+        <form method="post" action="studentReport.php" >
+        <h2>Please enter student ID</h2>
+        <input name = "srID" type = "int" size = 2 />
+        <input type = "submit" value = "view courses of this student">
+        </form>
+
+        <!--a list of students in a certain course -->
+        <form method="post" action="courseReport.php" >
+        <h2>Please enter course ID</h2>
+        <input name = "crID" type = "int" size = 2 />
+        <input type = "submit" value = "view student on this course">
+        </form>
 
     </body>
 </html>
